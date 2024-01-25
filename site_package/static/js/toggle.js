fac27c96342cef5d312858d07fec7429ec4903be
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-01-25 20:55:06 +0200

    Add project files
    - added project base files
    - created side structure
    - created side style
    - created side toggle & button active

---
+++ site_package/static/js/toggle.js
@@ -0,0 +1,18 @@
+const aside = document.getElementById("side"),
+  sideToggleButton = document.getElementById("side-toggle"),
+  tasksContainer = document.getElementById("tasks-container"),
+  sideButtons = document.querySelectorAll(".buttons-container div");
+
+sideToggleButton.onclick = (_) => aside.classList.toggle("hid-xe");
+tasksContainer.onclick = (_) => aside.classList.toggle("hid-xe");
+
+function activeSideButton() {
+  for (let i = 0; i < sideButtons.length; i++) {
+    sideButtons[i].classList.remove("active");
+  }
+  this.classList.add("active");
+}
+
+for (let i = 0; i < sideButtons.length; i++) {
+  sideButtons[i].addEventListener("click", activeSideButton);
+}
